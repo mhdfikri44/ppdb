4097c12889f39fe2b60d1c9d5da8dd8b6fec488b
mhdfikri44 <mhdfikr44@gmail.com>
2026-05-18 09:49:16 +0700

    modifier formulir margin

--- resources/views/pdf/formulir-penerimaan.blade.php
+++ resources/views/pdf/formulir-penerimaan.blade.php
@@ -7,7 +7,7 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon.ico') }}" />
     <style>
         @page {
-            margin-top: 7mm;
+            margin-top: 8mm;
             margin-bottom: 4mm;
         }
 
